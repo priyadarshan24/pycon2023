aaed11574820fbf28eb85b87c1a82e31aadb39b0
priyadarshanp <priyadarshanp@sahaj.ai>
2023-09-19 00:58:34 +0530

    Updating the consumer

--- PyconConsumer/locustfile.py
+++ PyconConsumer/locustfile.py
@@ -1,7 +1,5 @@
-from locust import HttpUser, task, constant, TaskSet
-import json
+from locust import HttpUser, task, constant, TaskSet, events
 from confluent_kafka import Producer, Consumer
-import random
 
 print("Hello, world!")
 
@@ -18,12 +16,18 @@ class KafkaConsumerUser(HttpUser):
             'enable.auto.commit': 'true',
         })
 
-        topic = 'my-topic'  # Replace with the topic you want to consume from
+        topic = 'my-topic-v3'  # Replace with the topic you want to consume from
         consumer.subscribe([topic])
 
-        message = consumer.poll(timeout=40)
-        print("message", message.value())
+        message = consumer.poll(timeout=20)
+        consumer.consume()
+
         if message:
-            print(message.value)
+            print("message##", message.value)
+            print("latency##", message.latency.)
+            events.request.fire(request_type="DEQUEUE",
+                                name="my-topic-v3",
+                                response_time=message.latency() * 1000,
+                                response_length=len(message.value()))
 
         consumer.close()
